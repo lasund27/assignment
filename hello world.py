cf554abb4eaa518bcb979f7ddd9e5f124b035b80
lasund27 <lasund27@gmail.com>
2025-10-19 03:15:16 +0900

    my first commit with three files

---
+++ hello world.py
@@ -0,0 +1,5 @@
+#This file is made for assignment
+
+print("Hello World")
+
+print("It is good day to die")
---
+++ i like programming.py
@@ -0,0 +1,5 @@
+#For study
+
+print("I want to be a programmer")
+print("Visual studio code is good to program in python")
+print("I love programming")
